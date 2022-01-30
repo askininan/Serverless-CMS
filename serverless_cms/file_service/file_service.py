009538b87b6dd3af9db3f70f2862fa54e4199af4
askininan <askin.inan@outlook.com>
2022-01-30 20:47:49 +0300

    Fixed file_service base64 issue, now can upload objects to s3

--- serverless_cms/file_service/file_service.py
+++ serverless_cms/file_service/file_service.py
@@ -1,9 +1,6 @@
 import base64
 import boto3
-
-
-# Initialize s3 boto3 object
-s3 = boto3.resource('s3')
+import json
 
 response  = {
     'statusCode': 200,
@@ -17,11 +14,17 @@ response  = {
 def lambda_handler(event, context):
 
     file_name = event['headers']['file-name']
+    print(file_name)
+    print (base64.b64decode(event['body']))
+    
     file_content = base64.b64decode(event['body'])
+    
+    # Initialize s3 boto3 object
+    s3 = boto3.resource('s3')
     
     # Set s3 table name variable
     BucketName = "serverless-cms-cmscontentbucket2170626e-1l96779yr71sn"
-    bucket = s3.Table(BucketName)
+    bucket = s3.Bucket(BucketName)
 
     try:
         s3_response = bucket.put_object(Bucket=BucketName, Key=file_name, Body=file_content)   
